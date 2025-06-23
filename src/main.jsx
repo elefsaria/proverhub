7a65f8e9e2d7772800f27bc5ed407078523082c5
elefsaria <mitraponsel5828@gmail.com>
2025-06-24 02:02:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles.css';
 
+// Halaman & Komponen
 import Login from './pages/Login';
 import Desktop from './pages/Desktop';
 import QuickMath from './components/QuickMath';
@@ -10,6 +11,7 @@ import CatClicker from './components/CatClicker';
 import FlappyBird from './components/FlappyBird';
 import MiniMario from './components/MiniMario';
 import Leaderboard from './pages/Leaderboard';
+import NFT from './pages/NFT'; // 🆕 Halaman NFT
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -22,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/flappy" element={<FlappyBird />} />
         <Route path="/mario" element={<MiniMario />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/nft" element={<NFT />} /> {/* 🆕 Tambahkan halaman NFT */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
