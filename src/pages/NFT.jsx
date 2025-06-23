b8e1bc43e160a64c2c720ce9ef34d5cc7a23445a
elefsaria <mitraponsel5828@gmail.com>
2025-06-24 02:00:22 +0700

    Create NFT.jsx

---
+++ src/pages/NFT.jsx
@@ -0,0 +1,9 @@
+import NFTGenerator from "../components/NFTGenerator";
+
+export default function NFT() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-100 via-pink-300 to-pink-500 p-6">
+      <NFTGenerator />
+    </div>
+  );
+}
